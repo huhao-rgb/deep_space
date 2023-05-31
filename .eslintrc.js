d519fcf5eb6406aab1dc5e9fbd292d97be9f283f
胡浩 <528201420@qq.com>
2023-05-31 15:03:29 +0800

    feat: 配置eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,7 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended'],
   plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-      },
-    },
-  ],
-};
+  rules: {}
+}
--- app/Navigation.tsx
+++ app/Navigation.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react'
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import shallow from 'zustand/shallow'
+import { shallow } from 'zustand/shallow'
 
 import TabNavigator from './TabNavigator'
 
